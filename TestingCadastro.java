e03534bfd710fee5cb2e279f32629c2c60416375
Mauricio Mello Martins <80491303+mauTins@users.noreply.github.com>
2022-09-12 16:05:31 -0300

    Update TestingCadastro.java

--- TestingCadastro.java
+++ TestingCadastro.java
@@ -11,46 +11,69 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
 
-public class TestingCadastro {
+public class BuscaNoBlog {
 
 	@Test
 
-	public void testeTextField()	{
+	public void AcessarAfuncionalidade() {
+			
+			
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\mauri\\OneDrive - Moochies Trading LTD\\Documentos\\chromedriver.exe");	
+//		System.setProperty("webdriver.gecko.driver","C:\\Users\\mauri\\OneDrive\\Documentos\\geckodriver.exe");	
+		WebDriver driver = new ChromeDriver();
+//		WebDriver driver = new FirefoxDriver();
+		driver.get("https://ww2.soc.com.br/blog/");
+		driver.findElement(By.id("sm-16630073058074175-3")).click();
+		driver.findElement(By.id("elementor-sub-item elementor-item-active")).click();
+
+		}
 		
-	System.setProperty("webdriver.chrome.driver","C:\\Users\\mauri\\OneDrive\\Documentos\\chromedriver.exe");	
-//	System.setProperty("webdriver.gecko.driver","C:\\Users\\mauri\\OneDrive\\Documentos\\geckodriver.exe");	
-	WebDriver driver = new ChromeDriver();
-//	WebDriver driver = new FirefoxDriver();
-	driver.get("https://wcaquino.me/selenium/componentes.html");
-	
-	driver.findElement(By.id("elementosForm:nome")).sendKeys("Mauricio");
-	driver.findElement(By.id("elementosForm:sobrenome")).sendKeys("Mello");
-	driver.findElement(By.id("elementosForm:sexo:0")).click();
-	driver.findElement(By.id("elementosForm:comidaFavorita")).click();
-	
-	new Select (driver.findElement(By.id("elementosForm:escolaridade")))
-			.selectByVisibleText ("Superior");
-	new Select (driver.findElement(By.id("elementosForm:esportes")))
-			.selectByVisibleText ("Corrida");
-	
-	driver.findElement(By.id("elementosForm:cadastrar")).click();
-
-	
-	Assert.assertTrue(driver.findElement(By.id("resultado")).getText().startsWith("Cadastrado!"));
-	Assert.assertTrue(driver.findElement(By.id("descNome")).getText().endsWith("Mauricio"));
-	Assert.assertEquals("Sobrenome: Mello", driver.findElement(By.id("descSobrenome")).getText());
-	Assert.assertEquals("Sexo: Masculino", driver.findElement(By.id("descSexo")).getText());
-	Assert.assertEquals("Comida: Pizza", driver.findElement(By.id("descComida")).getText());
-	Assert.assertEquals("Escolaridade: superior", driver.findElement(By.id("descEscolaridade")).getText());
-	Assert.assertEquals("Esportes: Corrida", driver.findElement(By.id("descEsportes")).getText());
-	
-
+	public void AcessaraRedeSoc() {
+		
+		
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\mauri\\OneDrive - Moochies Trading LTD\\Documentos\\chromedriver.exe");	
+//		System.setProperty("webdriver.gecko.driver","C:\\Users\\mauri\\OneDrive\\Documentos\\geckodriver.exe");	
+		WebDriver driver = new ChromeDriver();
+//		WebDriver driver = new FirefoxDriver();
+		driver.get("https://ww2.soc.com.br/blog/");
+		driver.findElement(By.id("sm-16630073058074175-3")).click();
+		driver.findElement(By.id("linkText=Rede SOCNET")).click();
+		
 	
 	}
-
+	public void BuscarCredenciados () {
+		
+		
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\mauri\\OneDrive - Moochies Trading LTD\\Documentos\\chromedriver.exe");	
+//		System.setProperty("webdriver.gecko.driver","C:\\Users\\mauri\\OneDrive\\Documentos\\geckodriver.exe");	
+		WebDriver driver = new ChromeDriver();
+//		WebDriver driver = new FirefoxDriver();
+		driver.get("https://ww2.soc.com.br/blog/");
+		driver.findElement(By.id("div-filtro-conveniencias .elemento-filtro:nth-child(4) .cbx-estilizado")).click();
+		driver.findElement(By.id("div-filtro-conveniencias .elemento-filtro:nth-child(5) .cbx-estilizado")).click();
+		driver.findElement(By.id("filtro-servicos .elemento-filtro:nth-child(1) .cbx-estilizado")).click();
+		driver.findElement(By.id("div-filtro-servicos .elemento-filtro:nth-child(2) .cbx-estilizado")).click();
+		driver.findElement(By.id("div-filtro-servicos .elemento-filtro:nth-child(3) .cbx-estilizado")).click();
+		driver.findElement(By.id("div-filtro-servicos .elemento-filtro:nth-child(4) .cbx-estilizado")).click();
+		driver.findElement(By.id("div-filtro-servicos .elemento-filtro:nth-child(5) .cbx-estilizado")).click();
+		driver.findElement(By.id("PlacesAutocomplete__suggestion-ChIJtwPWFT0CzpQRImEpQPbHKtA")).click();
+			
+	}
+	public void BuscarPerfil () {
+		
+		
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\mauri\\OneDrive - Moochies Trading LTD\\Documentos\\chromedriver.exe");	
+//		System.setProperty("webdriver.gecko.driver","C:\\Users\\mauri\\OneDrive\\Documentos\\geckodriver.exe");	
+		WebDriver driver = new ChromeDriver();
+//		WebDriver driver = new FirefoxDriver();
+		driver.get("https://ww2.soc.com.br/blog/");
+		driver.findElement(By.id("ipt-busca-credenciado-2")).sendKeys("11025-300");
+		driver.findElement(By.id("botao-buscar")).click();
+		driver.findElement(By.id("filtro-conveniencias .elemento-filtro:nth-child(4)")).click();
+		driver.findElement(By.id("filtro-conveniencias .elemento-filtro:nth-child(1)")).click();
+		
+	}
+		
 }
-
-
-
-
-
+	
+	
